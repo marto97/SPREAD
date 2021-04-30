66ec2182048df50492b7704f7dceb565ff7cb878
marto97 <Myn4868n5310>
2021-04-30 08:42:13 +0200

    added description for SIR and CanvasView.kt

---
+++ app/src/main/java/com/example/spread/CanvasView.kt
@@ -0,0 +1,11 @@
+package com.example.spread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CanvasView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_canvas_view)
+    }
+}
