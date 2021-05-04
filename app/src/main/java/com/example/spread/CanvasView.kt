e4bb77c49aa052a586cd5d68fcf0286ddd3ee2b3
marto97 <Myn4868n5310>
2021-05-04 09:42:49 +0200

    the circle is now moving and bouncing in CanvasView.kt

--- app/src/main/java/com/example/spread/CanvasView.kt
+++ app/src/main/java/com/example/spread/CanvasView.kt
@@ -2,6 +2,7 @@ package com.example.spread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CanvasView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,23 @@ class CanvasView : AppCompatActivity() {
         actionBar!!.title = "SIR Model Simulation"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val animationThread = Thread {
+            try {
+                var startTime: Long
+                var endTime: Long
+                while (true) {
+                    startTime = System.currentTimeMillis()
+                    (findViewById<View>(R.id.ball) as BallView).moveBall()
+                    findViewById<View>(R.id.ball).postInvalidate()
+                    endTime = System.currentTimeMillis()
+                    Thread.sleep(16 - (endTime - startTime))
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+
+        animationThread.start()
     }
 }
