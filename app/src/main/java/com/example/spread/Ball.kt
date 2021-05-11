72d9f14943ba602610b13da2f70086b4461b4eed
marto97 <Myn4868n5310>
2021-05-11 21:46:13 +0200

    add Ball.kt

---
+++ app/src/main/java/com/example/spread/Ball.kt
@@ -0,0 +1,51 @@
+package com.example.spread
+
+import android.graphics.Canvas
+import android.graphics.Paint
+
+
+class Ball (_xPos: Float, _yPos: Float, _width: Int, _height: Int, _paint: Paint){
+    // property (data member)
+    private var xPos: Float = _xPos
+    private var yPos: Float = _yPos
+    private var width = _width
+    private var height = _height
+    private var vX = 10F
+    private var vY = 10F
+
+    private val paint: Paint = _paint
+
+    private var ballRadius: Float = 30F
+
+    private fun moveBall() {
+        xPos += vX
+        yPos += vY
+
+        if(xPos > width - ballRadius) {   //Ball exited the right margin
+            val overshoot = xPos - (width - ballRadius)
+            xPos -= overshoot * 2
+            vX = -vX
+
+        } else if (xPos < ballRadius) {   //Ball exited the left border
+            val overshoot = ballRadius - xPos
+            xPos += overshoot * 2
+            vX = -vX
+        }
+
+        if(yPos > height - ballRadius) {  //Ball exited below
+            val overshoot = yPos - (height - ballRadius)
+            yPos -= overshoot * 2
+            vY = -vY
+
+        } else if(yPos < ballRadius) {    //Ball exited above
+            val overshoot = ballRadius - yPos
+            yPos += overshoot * 2
+            vY = -vY
+        }
+    }
+
+    fun draw(canvas: Canvas?){
+        moveBall()
+        canvas!!.drawCircle(xPos, yPos, ballRadius, paint)
+    }
+}
