1e5540bfd80023a0ec507eeae48adc026be1a357
marto97 <martinyankow@gmail.com>
2021-07-01 01:50:45 +0200

    init dots model calculation

---
+++ app/src/main/java/com/favoway/spread/ConfigureDotsModelActivity.kt
@@ -0,0 +1,119 @@
+package com.favoway.spread
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
+class ConfigureDotsModelActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configure_dots_model)
+
+        val susceptibleSeekBar: SeekBar = findViewById(R.id.susceptibleSeekBarR)
+        val susceptibleNumber: TextView = findViewById(R.id.susceptibleNumberR)
+
+        val sumDots: TextView = findViewById(R.id.sumDots)
+
+
+        var susceptibleInt = 25
+
+        val infectedSeekBar: SeekBar = findViewById(R.id.infectedSeekBarR)
+        val infectedNumber: TextView = findViewById(R.id.infectedNumberR)
+
+        var infectedInt = 25
+
+            susceptibleSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                susceptibleNumber.text = progress.toString()
+                susceptibleInt = progress
+                sumDots.text = (susceptibleInt*infectedInt).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+
+
+        infectedSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                infectedNumber.text = progress.toString()
+                infectedInt = progress
+                sumDots.text = (susceptibleInt*infectedInt).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        val recoveredSeekBar: SeekBar = findViewById(R.id.recoveredSeekBarR)
+        val recoveredNumber: TextView = findViewById(R.id.recoveredNumberR)
+
+        var recoveredInt = 25
+
+        recoveredSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                recoveredNumber.text = progress.toString()
+                recoveredInt = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        val rNumberSeekBar: SeekBar = findViewById(R.id.RNumberSeekBarR)
+        val rNumberNumber: TextView = findViewById(R.id.RNumberR)
+
+        //var rNumberInt = 1
+
+        rNumberSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val value = (progress.toFloat() / 10.0).toFloat()
+                rNumberNumber.text = value.toString()
+                //rNumberInt = value.toInt()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        val actionBar = supportActionBar
+
+        actionBar!!.title = "R Number"
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val startBtnR = findViewById<Button>(R.id.startBtnR)
+        startBtnR.setOnClickListener {
+            val intent = Intent(this@ConfigureDotsModelActivity, CanvasViewR::class.java)
+            intent.putExtra("susceptibleInt",susceptibleInt)
+            intent.putExtra("infectedInt",infectedInt)
+            intent.putExtra("recoveredInt",recoveredInt)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/favoway/spread/IntroDotsModel.kt
+++ app/src/main/java/com/favoway/spread/IntroDotsModel.kt
@@ -1,8 +1,10 @@
 package com.favoway.spread
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.widget.Button
 import android.widget.TextView
 
 class IntroDotsModel : AppCompatActivity() {
@@ -17,6 +19,12 @@ class IntroDotsModel : AppCompatActivity() {
         actionBar.setDisplayHomeAsUpEnabled(true)
 
         setupHyperlink()
+
+        val nextBtnDots = findViewById<Button>(R.id.nextBtnDots)
+        nextBtnDots.setOnClickListener {
+            val intent = Intent(this, ConfigureDotsModelActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun setupHyperlink() {
